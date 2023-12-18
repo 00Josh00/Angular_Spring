63a12c3c5b9cdbaa2cf561c485772cee143e30e7
00Josh00 <josephchaparro08@gmail.com>
2023-12-18 03:09:51 -0500

    Metodos update y delete

--- Spring/spring-boot-backend-apirest/src/main/java/com/josh/springboot/backend/apirest/controllers/PersonalInfoRestController.java
+++ Spring/spring-boot-backend-apirest/src/main/java/com/josh/springboot/backend/apirest/controllers/PersonalInfoRestController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -42,5 +44,27 @@ public class PersonalInfoRestController {
 	public PersonalInfo create( @RequestBody PersonalInfo personalinfo) {
 		return personalInfoService.save(personalinfo);
 	}
+	
+	
+	@PutMapping("/personalinfo/{id}")
+	public PersonalInfo update(@RequestBody PersonalInfo personalinfo, @PathVariable Long id) {
+		PersonalInfo personalinfoActual = personalInfoService.findById(id);
+		
+		personalinfoActual.setNombre(personalinfo.getNombre());
+		personalinfoActual.setApellidos(personalinfo.getApellidos());
+		personalinfoActual.setEmail(personalinfo.getEmail());
+		personalinfoActual.setTelefono(personalinfo.getEmail());
+		personalinfoActual.setDireccion(personalinfo.getEmail());
+		
+		
+		return personalInfoService.save(personalinfoActual);
+	}
+	
+	@DeleteMapping("/personalinfo/{id}")
+	public void delete(@PathVariable Long id) {
+		personalInfoService.delete(id);
+	}
+	
+	
 	
 }
