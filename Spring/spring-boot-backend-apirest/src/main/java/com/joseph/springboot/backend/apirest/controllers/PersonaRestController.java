28b35583ead96fc6649f5c84eb31f633c9b87a83
00Josh00 <josephchaparro08@gmail.com>
2023-12-19 14:22:19 -0500

    Update y Delete en API Rest

--- Spring/spring-boot-backend-apirest/src/main/java/com/joseph/springboot/backend/apirest/controllers/PersonaRestController.java
+++ Spring/spring-boot-backend-apirest/src/main/java/com/joseph/springboot/backend/apirest/controllers/PersonaRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -40,6 +42,27 @@ public class PersonaRestController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Persona create(@RequestBody Persona persona) {
 		return personaService.save(persona);
+	}
+	
+	@PutMapping("/personas/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Persona update(@RequestBody Persona persona, @PathVariable Long id) {
+		Persona personaActual = personaService.findById(id);
+		
+		personaActual.setNombre(persona.getNombre());
+		personaActual.setApellidos(persona.getApellidos());
+		personaActual.setDireccion(persona.getDireccion());
+		personaActual.setEmail(persona.getEmail());
+		personaActual.setTelefono(persona.getTelefono());
+		personaActual.setDireccion(persona.getDireccion());
+		
+		return personaService.save(personaActual);
+	}
+	
+	@DeleteMapping("/personas/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		personaService.delete(id);
 	}
 
 }
