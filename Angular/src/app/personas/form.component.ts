24f6c9186b2ae68e43af4d9b0336e829c82232ca
00Josh00 <josephchaparro08@gmail.com>
2023-12-19 15:44:03 -0500

    Implementando crear en Cliente Service

--- Angular/src/app/personas/form.component.ts
+++ Angular/src/app/personas/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from './persona';
+import { PersonaService } from './persona.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -9,10 +11,13 @@ export class FormComponent implements OnInit {
   public persona: Persona = new Persona();
   public titulo: string = 'Agregar Personas';
 
+  constructor(private personaService: PersonaService, private router: Router) {}
+
   ngOnInit(): void {}
 
   public create(): void {
-    console.log('Clicked!');
-    console.log(this.persona);
+    this.personaService
+      .create(this.persona)
+      .subscribe((reponse) => this.router.navigate(['/personas']));
   }
 }
--- Angular/src/app/personas/persona.service.ts
+++ Angular/src/app/personas/persona.service.ts
@@ -3,15 +3,23 @@ import { PERSONAS } from './personas.js.js';
 import { Persona } from './persona.js';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs';
 @Injectable()
 export class PersonaService {
   private urlEndPoint: string = 'http://localhost:8080/api/personas';
+
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
   constructor(private http: HttpClient) {}
 
   getPersonas(): Observable<Persona[]> {
     //return of(PERSONAS);
     return this.http.get<Persona[]>(this.urlEndPoint);
+  }
+
+  create(persona: Persona): Observable<Persona> {
+    return this.http.post<Persona>(this.urlEndPoint, persona, {
+      headers: this.httpHeaders,
+    });
   }
 }
