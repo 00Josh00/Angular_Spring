ed0b5be3c6d05617eeaaf8d810cac2189815a43e
00Josh00 <josephchaparro08@gmail.com>
2023-12-20 17:29:27 -0500

    Cambiando time zone
    de America/Peru a America/Lima

--- Spring/spring-boot-backend-apirest_old/src/main/java/com/josh/springboot/backend/apirest/models/services/IPersonalInfoService.java
+++
@@ -1,9 +0,0 @@
-package com.josh.springboot.backend.apirest.models.services;
-
-import java.util.List;
-
-import com.josh.springboot.backend.apirest.models.entity.PersonalInfo;
-
-public interface IPersonalInfoService {
-	public List<PersonalInfo> findAll();
-}
